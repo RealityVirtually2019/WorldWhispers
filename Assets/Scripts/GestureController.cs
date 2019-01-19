3f4f33bbc797b5ee0f3b41f68356fe64459b5907
Peter Iordanov <piordanov@fulcrum-gt.com>
2019-01-19 11:32:27 -0500

    example douple tap gesture

---
+++ Assets/Scripts/GestureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GestureController : MonoBehaviour
+{
+    public void Ping()
+    {
+        Debug.Log("Ping");
+    }
+
+    public void Vibrate() {
+        Handheld.Vibrate();
+    }
+}
